e1a8346b519bdb766b6c71ca422b8b957ce8d380
korhankosece <korhankosece@gmail.com>
2023-03-04 14:22:18 +0300

    JS Intro eklendi

---
+++ Intro/app.js
@@ -0,0 +1,26 @@
+console.log(1);
+console.log('Korhan');
+console.log("Korhan");
+
+// alert('Selam ben bir modalım.');
+
+
+// !!değişken isimlendirmeleri anlamlı olmalı ve işlevini net bir şekilde göstermeli
+// var x = 'asdasd' !!
+
+// camelcase naming convention değişken ve fonksiyonlarda kullanılır
+var nameSurname = 'Korhan Kösece';
+
+console.log(nameSurname);
+
+// JS kebap case kullanılamaz
+// var name-surname;
+
+// pascalcase naming convention class isimlendirmede kullanılır
+// class NameSurname;
+
+// snake case naming convention js de çok kullanılmaz
+// var total_count = 4;
+
+// screaming snake case naming convention SABİT DEĞİŞKENLERDE kullanılır
+// var TOTAL_COUNT = 4;
